e4c43688a084658a3f484755059fd4d640a92a5c
Francisco Spaulding-Astudillo <fspauldinga@ucla.edu>
2022-08-23 09:33:49 -0700

    Update mop.py

--- mop.py
+++ mop.py
@@ -6,6 +6,25 @@ import numpy as np
 # Create parameter class
 
 class parameters:
+    # Spatial grid
+    Rp   = 6314e3
+    ndeg = 180
+    ddeg = 1
+    dx   = Rp*np.pi/ndeg
+    x    = np.arange(0,ndeg+ddeg,ddeg)*dx
+    xlen = len(x)
+    # Temporal grid
+    dt   = 0.01
+    tmin = 0
+    tmax = 1
+    t    = np.arange(tmin,tmax+dt,dt)
+    tlen = len(t)
+    
+    # CFL criterion must be met
+    if not dt/dx < 0.4:
+        print('CFL criterion is not met.')
+        quit()
+    
     def __init__(self):
         self.Ts = []
         self.ps = []
@@ -20,16 +39,16 @@ class parameters:
         self.xidot = []
   
     def set_Ts(self, x): 
-        self.Ts.append(x) # see python list methods
+        # see python list methods
+        self.Ts.clear()
+        self.Ts.append(x)
     
+# Test: Create a class, setup Ts as any array, and then access the last element of Ts   
 par = parameters()
-x = np.ones([3],dtype='float') # suppose this is the new array for Ts
-par.set_Ts(x) # we set Ts
-
-# To access the array, print(par.Ts[0])
-# However, how do you access a single value of the np array?
-print(par.Ts[0])    # returns [1. 1. 1.]
-print(par.Ts[0][0]) # returns 1.0, but is not very clean
+Ts = np.arange(parameters.xlen,dtype='float') 
+par.set_Ts(Ts) 
+# print(par.Ts[0])      # returns the entire array
+# print(par.Ts[0][-1])  # returns the last value of the array 
 
 
       
