7fa091e5b81834989d6b36038473f1e3c91ddc0f
Francisco Spaulding-Astudillo <fspauldinga@ucla.edu>
2022-10-04 20:05:27 -0700

    Update mop.py

--- mop.py
+++ mop.py
@@ -9,7 +9,6 @@
 # Import python libraries
 import numpy as np
 import matplotlib.pyplot as plt 
-import math
 
 # Create parameter class
 
@@ -30,9 +29,9 @@ class parameters:
     jrb   = xlen-1
     jlrb = [jlb,jlb+1,jrb-1,jrb]
     # Temporal grid
-    dt   = 0.2*dx
+    dt   = 180 # seconds
     tmin = 0
-    tmax = dt*5
+    tmax = dt*1
     t    = np.arange(tmin,tmax+dt,dt)
     tlen = len(t)
     # CFL criterion must be met
@@ -52,8 +51,12 @@ class parameters:
     p0  = 2e4    # Pa
     g   = 10     # m/s^2
     tau = dt*100 # damping timescale (s)
-    # Surface Energy Imbalance (+- 5 W/m^2)
-    Fnet = np.cos(np.pi*np.arange(0,ndeg+ddeg,ddeg)/ndeg)*5
+
+    #plt.clf()
+    #fig,ax=plt.subplots()
+    #ax.plot(x/rp,Fnet)
+    #ax.set_ylabel('Fnet')
+    #plt.show()
     
     def __init__(self):
         self.Ts , self.ps   , self.rhodel, self.xidotdel,\
@@ -61,7 +64,14 @@ class parameters:
         self.rho, self.delta, self.xidot , self.Fnet    ,\
         self.Cm , self.Cu   , self.Ce    , self.ubar     \
         = (np.zeros([parameters.tlen,parameters.xlen],dtype='float') for i in range(16))
-                
+
+# Surface Energy Imbalance
+def get_Fnet():
+    # Specified at current time step, i
+    Fnet = np.zeros([par.xlen],dtype='float')
+    Fnet = np.cos(np.pi*np.arange(0,par.ndeg+par.ddeg,par.ddeg)/par.ndeg)*5 
+    return Fnet
+
 # Saturation Vapor Pressure
 def get_esat(x):
     # Input x could be T or Ts
@@ -83,8 +93,8 @@ def get_Cu():
             Cu[j] = -par.rho[i,j]*par.delta[i,j]*par.u[i,j]/par.tau
             
         else:
-            Cu[j] = -par.rho[i,j]  *par.delta[i,j]*par.u[i,j]/par.tau + \
-                     par.xidot[i,j]*par.delta[i,j]*par.u[i,j]
+            Cu[j] = -par.rho[i,j]  *par.delta[i,j]*par.u[i,j]/par.tau \
+                    +par.xidot[i,j]*par.delta[i,j]*par.u[i,j]
     return Cu       
     
 def get_Ce():
@@ -97,19 +107,13 @@ def get_Ce():
             Ce[j] = par.xidot[i,j]*par.delta[i,j]*par.e[i,j]     
     return Ce  
 
-def get_ubar():
-    # Diagnostic at current time step, i
-    ubar =  np.zeros([par.xlen],dtype='float')
-    #for j in range(par.jmin,par.jmaxp1): central difference?
-
-
 def get_Ts(): 
     # Prognostic
     Ts = np.zeros([par.xlen],dtype='float')
     Ts = par.Ts[i,:] + par.dt/par.cp*\
             (
              par.Fnet[i,:]-par.L*par.xidot[i,:]*par.delta[i,:]
-            )     
+            )   
     return Ts
 
 def get_ps(): 
@@ -124,7 +128,7 @@ def get_rhodel():
     # interior
     rhodel[par.jmin:par.jmaxp1] = (par.ps[i+1,par.jmin:par.jmaxp1]-par.p0)/par.g
     # left boundary
-    rhodel[par.jlb] = rhodel[par.jbl+1]
+    rhodel[par.jlb] = rhodel[par.jlb+1]
     # right boundary
     rhodel[par.jrb] = rhodel[par.jrb-1]
     return rhodel
@@ -135,7 +139,7 @@ def get_u():
     u = np.zeros([par.xlen],dtype='float')       # (t+1)
     # interior
     for j in range(par.jmin+1,par.jmax):
-        if par.u[i,j]>0:
+        if par.u[i,j]>=0:
             rhodelu[j] = par.rho[i,j]*par.delta[i,j]*par.u[i,j] +                                \
                          par.dt*(par.Cu[i,j] -                                                   
                                     (                                                                
@@ -164,7 +168,7 @@ def get_e(): # **
     # interior
     for j in range(par.jmin,par.jmaxp1):
         # insert criterion for direction of mean wind
-        if par.u[i,j]>0:
+        if par.u[i,j]>=0:
             rhodele[j] = par.rho[i,j]*par.delta[i,j]*par.e[i,j] +                                                  \
                          par.dt*(par.Ce[i,j] -                                                                     
                                     (                                                                             
@@ -221,33 +225,58 @@ def get_delta():
 def get_xidot(): # **
     # Diagnostic
     xidot = np.zeros([par.xlen],dtype='float') # (t+1)
-    for j in range(par.xlen):
-        if par.u[i,j]>0:
+    for j in range(par.jmin,par.jmaxp1):
+        if par.u[i,j]>=0:
             xidot[j] =  (
-                    par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
-                    + par.g*(                                                                 
-                            par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]    -              
-                            par.rho[i+1,j-1]*par.delta[i+1,j-1]*par.u[i+1,j-1]        
+                        par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
+                        + par.g*(                                                                 
+                            par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]              
+                            -par.rho[i+1,j-1]*par.delta[i+1,j-1]*par.u[i+1,j-1]        
                             )/(par.x[j]-par.x[j-1])
-                    )/\
-                    (                                                                       
-                    par.delta[i+1,j]*(par.g + 
-                                    (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp)
-                                    )
-                    )    
+                        )/\
+                        (                                                                       
+                        par.delta[i+1,j]*(par.g + 
+                                         (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp)
+                                         )
+                        )
         elif par.u[i,j]<0:
             xidot[j] =  (
-                    par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
-                    + par.g*(                                                                 
-                            par.rho[i+1,j+1]*par.delta[i+1,j+1]*par.u[i+1,j+1]    -              
-                            par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]        
+                        par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
+                        + par.g*(                                                                 
+                            par.rho[i+1,j+1]*par.delta[i+1,j+1]*par.u[i+1,j+1]           
+                           -par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]        
                             )/(par.x[j+1]-par.x[j])
-                    )/\
-                    (                                                                       
-                    par.delta[i+1,j]*(par.g + 
-                                    (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp)
-                                    )
-                    )    
+                        )/\
+                        (                                                                       
+                        par.delta[i+1,j]*(par.g + 
+                                         (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp)
+                                         )
+                        )    
+    # left boundary
+    j = par.jlb
+    xidot[j] =  (
+                par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
+               +par.g*(                                                                 
+                        par.rho[i+1,j+1]*par.delta[i+1,j+1]*par.u[i+1,j+1]           
+                        -par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]        
+                    )/(par.x[j+1]-par.x[j])
+                )/\
+                (                                                                       
+                par.delta[i+1,j]*(par.g + (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp))
+                )    
+    # right boundary
+    j = par.jrb
+    xidot[j] =  (
+                 par.Fnet[i+1,j]*(par.ps[i+1,j]*par.L)/(par.R*par.Ts[i+1,j]**2*par.cp)  
+                +par.g*(                                                                 
+                        par.rho[i+1,j]  *par.delta[i+1,j]  *par.u[i+1,j]              
+                        -par.rho[i+1,j-1]*par.delta[i+1,j-1]*par.u[i+1,j-1]        
+                        )/(par.x[j]-par.x[j-1])
+                )/\
+                (                                                                       
+                par.delta[i+1,j]*(par.g + (par.ps[i+1,j]*par.L**2)/(par.R*par.Ts[i+1,j]**2*par.cp))
+                )
+
     return xidot
 
 def get_initial_conditions():
@@ -255,55 +284,70 @@ def get_initial_conditions():
     par.Ts[0,:]       = np.ones([par.xlen],dtype='float')*350 # uniform initial temp
     par.ps[0,:]       = np.ones([par.xlen],dtype='float')*4e4 # uniform initial pressure
     par.rhodel[0,:]   = (par.ps[0,:]-par.p0)/par.g            # uniform atmospheric mass
-    par.u[0,:]        = np.zeros([par.xlen],dtype='float')    # no wind
+    par.u[0,:]        = np.ones([par.xlen],dtype='float')     # 1 m/s initial wind
+    par.u[0,par.jlb:par.jlb+2]   = 0 
+    par.u[0,par.jrb-1:par.jrb+1] = 0 
     par.T[0,:]        = par.Ts[0,:]                           # T = Ts
     par.e[0,:]        = par.cv*par.T[0,:] + 0.5*par.u[0,:]**2 
     par.p[0,:]        = par.ps[0,:]                           # p = ps
     par.rho[0,:]      = par.p[0,:]/(par.R*par.T[0,:])         
     par.delta[0,:]    = par.rhodel[0,:]/par.rho[0,:]          
-    par.xidot[i+1,:]  = np.zeros([par.xlen],dtype='float')    # zero surface mass-flux
+    par.xidot[0,:]    = np.zeros([par.xlen],dtype='float')    # zero surface mass-flux
+    #print(par.rhodel[0,:])
+    #print(par.e[0,:])
+    #print(par.p[0,:])
+    #print(par.rho[0,:])
+    #print(par.delta[0,:])
     
 # Time Integration
 par = parameters()
 get_initial_conditions()
 
 # Plot forward integration
-plt.clf()
-fig, ax = plt.subplots(7)    
+fig, ax = plt.subplots(7,2,figsize=(4,8))    
 
-for i in range(par.tlen):
+for i in range(par.tlen-1): 
     # Get variables at current time step
     par.Cm[i,:]         = get_Cm()
     par.Cu[i,:]         = get_Cu()
     par.Ce[i,:]         = get_Ce()
+    par.Fnet[i,:]       = get_Fnet()
     # Get variables at the next time step
-    par.Ts[i+1,:]       = get_Ts()       
-    par.ps[i+1,:]       = get_ps()       
+    par.Ts[i+1,:]       = get_Ts()    
+    par.ps[i+1,:]       = get_ps()    
     par.rhodel[i+1,:]   = get_rhodel()   
     par.u[i+1,:]        = get_u()        
-    par.e[i+1,:]        = get_e()        
-    par.T[i+1,:]        = get_T()        
+    par.e[i+1,:]        = get_e()   
+    par.T[i+1,:]        = get_T()
     par.p[i+1,:]        = get_p()        
     par.rho[i+1,:]      = get_rho()      
     par.delta[i+1,:]    = get_delta()   
     par.xidot[i+1,:]    = get_xidot()  
     
-    ax[0] = ax.plot(par.x/np.rp,par.T[i,:]) 
-    ax[1] = ax.plot(par.x/np.rp,par.Ts[i,:]) 
-    ax[2] = ax.plot(par.x/np.rp,par.p[i,:]) 
-    ax[3] = ax.plot(par.x/np.rp,par.ps[i,:]) 
-    ax[4] = ax.plot(par.x/np.rp,par.u[i,:])
-    ax[5] = ax.plot(par.x/np.rp,par.delta[i,:])
-    ax[6] = ax.plot(par.x/np.rp,par.xidot[i,:])
-    
-ax[0].set_xlabel('T')
-ax[1].set_xlabel('Ts')
-ax[2].set_xlabel('p')
-ax[3].set_xlabel('ps')
-ax[4].set_xlabel('u')
-ax[5].set_xlabel('delta')
-ax[6].set_xlabel('xidot')
+    ax[0,0].plot(par.x/par.rp,par.T[i,:]) 
+    ax[1,0].plot(par.x/par.rp,par.Ts[i,:]) 
+    ax[2,0].plot(par.x/par.rp,par.p[i,:]) 
+    ax[3,0].plot(par.x/par.rp,par.ps[i,:]) 
+    ax[4,0].plot(par.x/par.rp,par.u[i,:])
+    ax[5,0].plot(par.x/par.rp,par.delta[i,:])
+    ax[6,0].plot(par.x/par.rp,par.xidot[i,:])
 
+    ax[0,1].plot(par.x/par.rp,par.T[i+1,:]) 
+    ax[1,1].plot(par.x/par.rp,par.Ts[i+1,:]) 
+    ax[2,1].plot(par.x/par.rp,par.p[i+1,:]) 
+    ax[3,1].plot(par.x/par.rp,par.ps[i+1,:]) 
+    ax[4,1].plot(par.x/par.rp,par.u[i+1,:])
+    ax[5,1].plot(par.x/par.rp,par.delta[i+1,:])
+    ax[6,1].plot(par.x/par.rp,par.xidot[i+1,:])
+for c in range(0,2):    
+    ax[0,c].set_xlabel('T')
+    ax[1,c].set_xlabel('Ts')
+    ax[2,c].set_xlabel('p')
+    ax[3,c].set_xlabel('ps')
+    ax[4,c].set_xlabel('u')
+    ax[5,c].set_xlabel('delta')
+    ax[6,c].set_xlabel('xidot')
+plt.tight_layout()
 plt.show()
     
     
